4f82befdf9f86d1fe1c392262f9962b237dcb993
yemimasutanto <yemimasutanto28@gmail.com>
2020-02-22 17:44:12 +0700

    nambah halaman

---
+++ app/src/main/java/com/yemima/gardapanganmobile/EventGold3Activity.java
@@ -0,0 +1,14 @@
+package com.yemima.gardapanganmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventGold3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_gold3);
+    }
+}
---
+++ app/src/main/java/com/yemima/gardapanganmobile/SupportPageActivity.java
@@ -0,0 +1,14 @@
+package com.yemima.gardapanganmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SupportPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_support_page);
+    }
+}
